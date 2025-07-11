32a850139bc7c5ed51862682bb7b32ef186c1661
vuyolalapi <lalapivuyo26@gmail.com>
2025-07-11 13:42:12 +0200

    Add files via upload

---
+++ CaptureCars/src/com/cars/Car.java
@@ -0,0 +1,59 @@
+package com.cars;
+
+public class Car {
+    private String make;
+    private String model;
+    private String vin;
+    private String plateNumber;
+    private int mileage;
+    private int year;
+
+    // Getters and Setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
